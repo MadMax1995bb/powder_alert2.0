125c1f251dc2a0594a0ec063fc7aeaa708fa6358
Torsten Wrigley <wrigleytorsten@gmail.com>
2024-12-13 15:51:53 +0000

    changes

--- powderalert/api/fast.py
+++ powderalert/api/fast.py
@@ -14,11 +14,11 @@ from darts import TimeSeries
 
 app = FastAPI()
 
-# model_relative_path = 'models/my_model.pt'
-# app.state.model = TransformerModel.load(model_relative_path)
+model_relative_path = 'models/my_model.pt'
+app.state.model = TransformerModel.load(model_relative_path)
 
 max_model_relative_path = 'models/max_fullDS_all_features.keras'
-app.state.model = TransformerModel.load(max_model_relative_path)
+app.state.model2 = TransformerModel.load(max_model_relative_path)
 
 @app.get("/")
 def root():
@@ -26,11 +26,10 @@ def root():
     return dict(greeting="Hello")
     # $CHA_END
 
-
 @app.get("/predict_temp")
 def predict():
 
-    model = app.state.model
+    model = app.state.model2
     assert model is not None
 
     data = fetch_prediction_data(lat,long)
@@ -39,7 +38,6 @@ def predict():
     X_processed = preprocess(X_pred)
 
     data_darts = data.copy()
-    data_darts['date'] = data_darts['date'].dt.tz_localize(None)
 
     snowfall_series = TimeSeries.from_dataframe(data_darts, 'date', 'snowfall')
     feature_series = TimeSeries.from_dataframe(X_processed, value_cols=X_processed.columns)
@@ -57,31 +55,30 @@ def predict():
     # $CHA_END
 
 
-# @app.get("/predict_snowfall")
-# def predict():
+@app.get("/predict_snowfall")
+def predict():
 
-#     model = app.state.model
-#     assert model is not None
+    model = app.state.model
+    assert model is not None
 
-#     data = fetch_prediction_data(lat,long)
-#     cleaned_data = clean_data(data)
-#     X_pred = define_X(cleaned_data,target)
-#     X_processed = preprocess(X_pred)
+    data = fetch_prediction_data(lat,long)
+    cleaned_data = clean_data(data)
+    X_pred = define_X(cleaned_data,target)
+    X_processed = preprocess(X_pred)
 
-#     data_darts = data.copy()
-#     data_darts['date'] = data_darts['date'].dt.tz_localize(None)
+    data_darts = data.copy()
 
-#     snowfall_series = TimeSeries.from_dataframe(data_darts, 'date', 'snowfall')
-#     feature_series = TimeSeries.from_dataframe(X_processed, value_cols=X_processed.columns)
+    snowfall_series = TimeSeries.from_dataframe(data_darts, 'date', 'snowfall')
+    feature_series = TimeSeries.from_dataframe(X_processed, value_cols=X_processed.columns)
 
-#     print(X_processed)
-#     y_pred = model.predict(series =snowfall_series,past_covariates=feature_series,n=48)
+    print(X_processed)
+    y_pred = model.predict(series =snowfall_series,past_covariates=feature_series,n=48)
 
-#     print(y_pred)
-#     # ⚠️ fastapi only accepts simple Python data types as a return value
-#     # among them dict, list, str, int, float, bool
-#     # in order to be able to convert the api response to JSON
-#     return {
-#         'key': 'value'
-#     }
-#     # $CHA_END
+    print(y_pred)
+    # ⚠️ fastapi only accepts simple Python data types as a return value
+    # among them dict, list, str, int, float, bool
+    # in order to be able to convert the api response to JSON
+    return {
+        'key': 'value'
+    }
+    # $CHA_END
