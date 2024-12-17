f35ccbb9340a1f06b20e42bce937d99109a791d4
Max Burger <maxburger15@gmail.com>
2024-12-18 00:15:50 +0100

    Updates

--- powderalert/api/fast.py
+++ powderalert/api/fast.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from powderalert.ml_logic.data import fetch_prediction_data, clean_data
 from powderalert.ml_logic.preprocessor import preprocess
 from powderalert.ml_logic.params import *
-from powderalert.ml_logic.registry import load_model_snowfall, load_model_temperature
+from powderalert.ml_logic.registry import load_model_snowfall, load_model_temperature, load_model_snowfall2, load_model_windspeed
 from darts import TimeSeries
 from datetime import datetime, timedelta
 import numpy as np
@@ -12,6 +12,9 @@ app = FastAPI()
 
 app.state.model1 = load_model_snowfall()
 app.state.model2 = load_model_temperature()
+app.state.model3 = load_model_snowfall2()
+app.state.model4 = load_model_windspeed()
+
 
 @app.get("/")
 def root():
@@ -43,22 +46,11 @@ def predict(lat: float, long: float):
         'snowfall_prediction': y_pred
     }
 
-
 @app.get("/predict_temperature")
 def predict(lat: float, long: float):
     data = fetch_prediction_data(lat,long)
     cleaned_data = clean_data(data)
     df = preprocess(cleaned_data)
-
-    #####################################################
-    # The following individual steps where needed compared to the Darts model:
-    # if 'Unnamed: 0' in df.columns:
-    #     df = df.drop(columns=['Unnamed: 0'])
-    # current_time = datetime.now()
-    # last_48h = df[(df['date'] <= current_time) & (df['date'] > current_time - timedelta(hours=48))]
-    # last_48h = last_48h.drop(columns='date')
-    #####################################################
-    #Get time information
 
     first_predict_time = (pd.Timestamp(data.date.tail(1).values[0]) + pd.Timedelta(hours=1)).strftime("%Y-%m-%dT%H:00")
 
@@ -70,4 +62,45 @@ def predict(lat: float, long: float):
     return {
         'first_predict_time': first_predict_time,
         'temperature_prediction': next_48h
+    }
+
+###################################################################################
+########################## Optional Snowfall predictions ##########################
+
+@app.get("/predict_snowfall2")
+def predict(lat: float, long: float):
+    data = fetch_prediction_data(lat,long)
+    cleaned_data = clean_data(data)
+    df = preprocess(cleaned_data)
+
+    first_predict_time = (pd.Timestamp(data.date.tail(1).values[0]) + pd.Timedelta(hours=1)).strftime("%Y-%m-%dT%H:00")
+
+    last_48h = np.expand_dims(df, axis=0)
+    predictions = app.state.model3.predict(last_48h)
+    predicted_temperatures = predictions[0]
+    next_48h = [float(i) for i in predicted_temperatures]
+
+    return {
+        'first_predict_time': first_predict_time,
+        'snowfall2_prediction': next_48h
+    }
+
+################################### Optional DL wind prediction ##############################
+
+@app.get("/predict_windspeed")
+def predict(lat: float, long: float):
+    data = fetch_prediction_data(lat,long)
+    cleaned_data = clean_data(data)
+    df = preprocess(cleaned_data)
+
+    first_predict_time = (pd.Timestamp(data.date.tail(1).values[0]) + pd.Timedelta(hours=1)).strftime("%Y-%m-%dT%H:00")
+
+    last_48h = np.expand_dims(df, axis=0)
+    predictions = app.state.model4.predict(last_48h)
+    predicted_temperatures = predictions[0]
+    next_48h = [float(i) for i in predicted_temperatures]
+
+    return {
+        'first_predict_time': first_predict_time,
+        'windspeed_prediction': next_48h
     }
