da3bcfba5cecf1304ea38446af98e2e3cd7d77fd
Max Burger <maxburger15@gmail.com>
2024-12-18 16:21:15 +0100

    Updated models/ fast.py

--- powderalert/api/fast.py
+++ powderalert/api/fast.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from powderalert.ml_logic.data import fetch_prediction_data, clean_data
 from powderalert.ml_logic.preprocessor import preprocess
 from powderalert.ml_logic.params import *
-from powderalert.ml_logic.registry import load_model_snowfall, load_model_temperature, load_model_snowfall2, load_model_windspeed
+from powderalert.ml_logic.registry import load_model_temperature, load_model_snowdepth
 from darts import TimeSeries
 from datetime import datetime, timedelta
 import numpy as np
@@ -10,48 +10,34 @@ import pandas as pd
 
 app = FastAPI()
 
-app.state.model1 = load_model_snowfall()
-app.state.model2 = load_model_temperature()
-app.state.model3 = load_model_snowfall2()
-app.state.model4 = load_model_windspeed()
-
+app.state.model1 = load_model_temperature()
+app.state.model2 = load_model_snowdepth()
 
 @app.get("/")
 def root():
     return dict(greeting="Hello")
 
-@app.get("/predict_snowfall")
+@app.get("/predict_temperature")
 def predict(lat: float, long: float):
-
     data = fetch_prediction_data(lat,long)
     cleaned_data = clean_data(data)
+    df = preprocess(cleaned_data)
 
-    X = cleaned_data.drop(columns = target1)
-    y = cleaned_data[target1]
-    y = y.reset_index()
-
-    X_processed = preprocess(X)
-    df = y.join(X_processed)
-
-    #Get time information
     first_predict_time = (pd.Timestamp(data.date.tail(1).values[0]) + pd.Timedelta(hours=1)).strftime("%Y-%m-%dT%H:00")
 
-    X_pred_columns = X_processed.columns.tolist()
+    last_48h = np.expand_dims(df, axis=0)
+    predictions = app.state.model1.predict(last_48h)
+    predicted_temperatures = predictions[0]
+    next_48h = [float(i) for i in predicted_temperatures]
 
-    snowfall_series = TimeSeries.from_dataframe(df, 'date', value_cols=['snowfall']).astype("float32")
-    feature_series = TimeSeries.from_dataframe(df, 'date', value_cols=X_pred_columns).astype("float32")
-
-    y_pred = app.state.model1.predict(series=snowfall_series, past_covariates=feature_series, n=48).values().flatten().tolist()
-    # ⚠️ fastapi only accepts simple Python data types as a return value
-    # among them dict, list, str, int, float, bool
-    # in order to be able to convert the api response to JSON
-    # uvicorn simple:app --reload
     return {
         'first_predict_time': first_predict_time,
-        'snowfall_prediction': y_pred
+        'temperature_prediction': next_48h
     }
 
-@app.get("/predict_temperature")
+###################################################################################
+
+@app.get("/predict_snowdepth")
 def predict(lat: float, long: float):
     data = fetch_prediction_data(lat,long)
     cleaned_data = clean_data(data)
@@ -66,46 +52,36 @@ def predict(lat: float, long: float):
 
     return {
         'first_predict_time': first_predict_time,
-        'temperature_prediction': next_48h
+        'snowdepth_prediction': next_48h
     }
 
-###################################################################################
-########################## Optional Snowfall predictions ##########################
+# @app.get("/predict_snowfall")
+# def predict(lat: float, long: float):
 
-@app.get("/predict_snowfall2")
-def predict(lat: float, long: float):
-    data = fetch_prediction_data(lat,long)
-    cleaned_data = clean_data(data)
-    df = preprocess(cleaned_data)
+#     data = fetch_prediction_data(lat,long)
+#     cleaned_data = clean_data(data)
 
-    first_predict_time = (pd.Timestamp(data.date.tail(1).values[0]) + pd.Timedelta(hours=1)).strftime("%Y-%m-%dT%H:00")
+#     X = cleaned_data.drop(columns = target1)
+#     y = cleaned_data[target1]
+#     y = y.reset_index()
 
-    last_48h = np.expand_dims(df, axis=0)
-    predictions = app.state.model3.predict(last_48h)
-    predicted_temperatures = predictions[0]
-    next_48h = [float(i) for i in predicted_temperatures]
+#     X_processed = preprocess(X)
+#     df = y.join(X_processed)
 
-    return {
-        'first_predict_time': first_predict_time,
-        'snowfall2_prediction': next_48h
-    }
+#     #Get time information
+#     first_predict_time = (pd.Timestamp(data.date.tail(1).values[0]) + pd.Timedelta(hours=1)).strftime("%Y-%m-%dT%H:00")
 
-################################### Optional DL wind prediction ##############################
+#     X_pred_columns = X_processed.columns.tolist()
 
-@app.get("/predict_windspeed")
-def predict(lat: float, long: float):
-    data = fetch_prediction_data(lat,long)
-    cleaned_data = clean_data(data)
-    df = preprocess(cleaned_data)
+#     snowfall_series = TimeSeries.from_dataframe(df, 'date', value_cols=['snowfall']).astype("float32")
+#     feature_series = TimeSeries.from_dataframe(df, 'date', value_cols=X_pred_columns).astype("float32")
 
-    first_predict_time = (pd.Timestamp(data.date.tail(1).values[0]) + pd.Timedelta(hours=1)).strftime("%Y-%m-%dT%H:00")
-
-    last_48h = np.expand_dims(df, axis=0)
-    predictions = app.state.model4.predict(last_48h)
-    predicted_temperatures = predictions[0]
-    next_48h = [float(i) for i in predicted_temperatures]
-
-    return {
-        'first_predict_time': first_predict_time,
-        'windspeed_prediction': next_48h
-    }
+#     y_pred = app.state.model1.predict(series=snowfall_series, past_covariates=feature_series, n=48).values().flatten().tolist()
+#     # ⚠️ fastapi only accepts simple Python data types as a return value
+#     # among them dict, list, str, int, float, bool
+#     # in order to be able to convert the api response to JSON
+#     # uvicorn simple:app --reload
+#     return {
+#         'first_predict_time': first_predict_time,
+#         'snowfall_prediction': y_pred
+#     }
--- powderalert/ml_logic/registry.py
+++ powderalert/ml_logic/registry.py
@@ -6,32 +6,6 @@ from darts.models import TransformerModel
 from powderalert.ml_logic.params import MODEL_TARGET, LOCAL_REGISTRY_PATH
 import torch
 
-def load_model_snowfall():
-    """
-    Return a saved model:
-    - locally (latest one in alphabetical order)
-    - or from GCS (most recent one) if MODEL_TARGET=='gcs'  --> optional
-    Return None (but do not Raise) if no model is found
-    """
-
-    if MODEL_TARGET == "local":
-        print(Fore.BLUE + f"\nLoad latest model from local registry..." + Style.RESET_ALL)
-
-        # Get the latest model version name by the timestamp on disk
-        local_model_directory = os.path.join(LOCAL_REGISTRY_PATH, 'models', 'snowfall')
-        local_model_paths = glob.glob(f"{local_model_directory}/*.pt")
-
-        if not local_model_paths:
-            return None
-
-        most_recent_model_path_on_disk = max(local_model_paths, key=os.path.getmtime)
-
-        latest_model = TransformerModel.load(most_recent_model_path_on_disk, map_location=torch.device('cpu'))
-
-        print("✅ Model loaded from local disk")
-
-        return latest_model
-
 def load_model_temperature():
     """
     Return a saved model:
@@ -59,9 +33,8 @@ def load_model_temperature():
         return latest_model
 
 ###################################################################################
-########################## Optional Snowfall predictions ##########################
 
-def load_model_snowfall2():
+def load_model_snowdepth():
     """
     Return a saved model:
     - locally (latest one in alphabetical order)
@@ -73,7 +46,7 @@ def load_model_snowfall2():
         print(Fore.BLUE + f"\nLoad latest model from local registry..." + Style.RESET_ALL)
 
         # Get the latest model version name by the timestamp on disk
-        local_model_directory = os.path.join(LOCAL_REGISTRY_PATH, 'models', 'snowfall2')
+        local_model_directory = os.path.join(LOCAL_REGISTRY_PATH, 'models', 'snowdepth')
         local_model_paths = glob.glob(f"{local_model_directory}/*")
 
         if not local_model_paths:
@@ -87,30 +60,28 @@ def load_model_snowfall2():
 
         return latest_model
 
-################################### Optional DL wind prediction ##############################
+# def load_model_snowfall():
+#     """
+#     Return a saved model:
+#     - locally (latest one in alphabetical order)
+#     - or from GCS (most recent one) if MODEL_TARGET=='gcs'  --> optional
+#     Return None (but do not Raise) if no model is found
+#     """
 
-def load_model_windspeed():
-    """
-    Return a saved model:
-    - locally (latest one in alphabetical order)
-    - or from GCS (most recent one) if MODEL_TARGET=='gcs'  --> optional
-    Return None (but do not Raise) if no model is found
-    """
+#     if MODEL_TARGET == "local":
+#         print(Fore.BLUE + f"\nLoad latest model from local registry..." + Style.RESET_ALL)
 
-    if MODEL_TARGET == "local":
-        print(Fore.BLUE + f"\nLoad latest model from local registry..." + Style.RESET_ALL)
+#         # Get the latest model version name by the timestamp on disk
+#         local_model_directory = os.path.join(LOCAL_REGISTRY_PATH, 'models', 'snowfall')
+#         local_model_paths = glob.glob(f"{local_model_directory}/*.pt")
 
-        # Get the latest model version name by the timestamp on disk
-        local_model_directory = os.path.join(LOCAL_REGISTRY_PATH, 'models', 'windspeed')
-        local_model_paths = glob.glob(f"{local_model_directory}/*")
+#         if not local_model_paths:
+#             return None
 
-        if not local_model_paths:
-            return None
+#         most_recent_model_path_on_disk = max(local_model_paths, key=os.path.getmtime)
 
-        most_recent_model_path_on_disk = max(local_model_paths, key=os.path.getmtime)
+#         latest_model = TransformerModel.load(most_recent_model_path_on_disk, map_location=torch.device('cpu'))
 
-        latest_model = tf.keras.models.load_model(most_recent_model_path_on_disk)
+#         print("✅ Model loaded from local disk")
 
-        print("✅ Model loaded from local disk")
-
-        return latest_model
+#         return latest_model
